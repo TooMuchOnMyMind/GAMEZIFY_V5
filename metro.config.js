ecafeb5d9443fd9363131d083c81b8872dfa47b3
Harshad <harshadarvindh@gmail.com>
2025-03-02 11:48:40 -0500

    Lets see this

--- metro.config.js
+++
@@ -1,7 +0,0 @@
-import { getDefaultConfig } from "expo/metro-config";
-
-const defaultConfig = getDefaultConfig(import.meta.url);
-
-defaultConfig.resolver.sourceExts.push("web.js");
-
-export default defaultConfig;
